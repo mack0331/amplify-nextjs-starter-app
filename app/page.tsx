e1506571aede76e48111d16a0ffb28ea76c8bdd9
mack0331 <ericjmackay@gmail.com>
2023-11-21 16:04:31 -0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
+  return (<>
     <main className={styles.main}>
-      <div className={styles.description}>
+    <text>Hello, Jon.</text>
+      <div className={styles.center}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          This is what it looks like to test your thing.
         </p>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
         <a
           href="https://docs.amplify.aws/gen2/"
@@ -82,5 +62,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </>
   )
 }
